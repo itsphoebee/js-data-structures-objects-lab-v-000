e05f7df2f910656e311abbe77950cbac75c3bc29
Phoebe Lau <phoebelau7@gmail.com>
2018-03-10 00:32:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function destructivelyUpdateDriverWithKeyAndValue(obj, key, value){
 }
 function deleteFromDriverByKey(obj, key){
   const newObj = {...obj};
-  delete newObj.key;
+  delete newObj[key];
   return newObj;
 }
